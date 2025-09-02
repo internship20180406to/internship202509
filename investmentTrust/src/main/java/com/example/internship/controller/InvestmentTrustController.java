abc4ce01c88e0d91b1b9f4b98a72b718c3ed3c24
ffg-training <internship0216to0222@gmail.com>
2025-09-02 09:16:37 +0900

    修正

--- investmentTrust/src/main/java/com/example/internship/controller/InvestmentTrustController.java
+++ investmentTrust/src/main/java/com/example/internship/controller/InvestmentTrustController.java
@@ -9,6 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class InvestmentTrustController {
@@ -16,17 +23,32 @@ public class InvestmentTrustController {
     @Autowired
     private OrderInvestmentTrustService orderInvestmentTrustService;
 
+    private static final Map<String, List<String>> branchMap = Map.of(
+            "山陰共同銀行", List.of("牛肉支店", "豚肉支店", "鶏肉支店"),
+            "海陰共同銀行", List.of("おさかな支店", "海鮮丼支店", "くじら支店")
+    );
+
+
     @GetMapping("/investmentTrust")
     public String bankTransfer(Model model) {
         model.addAttribute("investmentTrustApplication", new InvestmentTrustForm());
-        model.addAttribute("nameOptions", "山陰共同銀行");
+        List<String> nameOptions = List.of("山陰共同銀行", "海陰共同銀行");
+        model.addAttribute("nameOptions", nameOptions);
         return "investmentTrustMain";
+    }
+
+    @GetMapping("/getBranches")
+    @ResponseBody
+    public List<String> getBranches(@RequestParam String bankName) {
+        return branchMap.getOrDefault(bankName, List.of());
     }
 
     @PostMapping("/investmentTrustConfirmation")
     public String confirmation(@ModelAttribute InvestmentTrustForm investmentTrustForm, Model model) {
-        investmentTrustForm.setBankName("ながれぼし銀行");
+        //investmentTrustForm.setBankName("ながれぼし銀行");
         model.addAttribute("bankName", investmentTrustForm.getBankName());
+        model.addAttribute("bankBranchName", investmentTrustForm.getBankBranchName());
+        model.addAttribute("bankKamokuName", investmentTrustForm.getBankKamokuName());
         model.addAttribute("bankAccountNum", investmentTrustForm.getBankAccountNum());
         model.addAttribute("investmentTrustApplication", investmentTrustForm);
         return "investmentTrustConfirmation";
--- investmentTrust/src/main/java/com/example/internship/entity/InvestmentTrustForm.java
+++ investmentTrust/src/main/java/com/example/internship/entity/InvestmentTrustForm.java
@@ -12,7 +12,20 @@ public class InvestmentTrustForm {
     @NonNull
     private String bankName;
     @NonNull
-    private Integer bankAccountNum;
+    private String bankAccountNum;
+    @NonNull
+    private String bankBranchName;
+    @NonNull
+    private String bankKamokuName;
+
+    @NonNull
+    private String bankAccountName;
+    @NonNull
+    private String brandName;
+
+    @NonNull
+    private String purchase;
+
 
     public String getBankName() {
         return bankName;
@@ -22,11 +35,50 @@ public class InvestmentTrustForm {
         this.bankName = bankName;
     }
 
-    public Integer getBankAccountNum() {
+    public String getBankAccountNum() {
         return bankAccountNum;
     }
 
-    public void setBankAccountNum(Integer bankAccountNum) {
+    public void setBankAccountNum(String bankAccountNum) {
         this.bankAccountNum = bankAccountNum;
     }
+
+    public String getBankBranchName() { return bankBranchName;}
+
+    public void setBankBranchName(String bankBranchName) {
+        this.bankBranchName = bankBranchName;
+    }
+
+    public String getBankKamokuName() { return bankKamokuName;}
+
+    public void setBankKamokuName(String bankKamokuName) {
+        this.bankKamokuName = bankKamokuName;
+    }
+
+    public String getBankAccountName() {
+        return bankAccountName;
+    }
+
+    public void setBankAccountName(String bankAccountName) {
+        this.bankAccountName = bankAccountName;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public String getPurchase() {
+        return purchase;
+    }
+
+    public void setPurchase(String purchase) {
+        this.purchase = purchase;
+    }
+
+
+
 }
