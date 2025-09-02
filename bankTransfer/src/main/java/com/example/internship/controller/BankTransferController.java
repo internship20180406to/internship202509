01ab9cc9371691ec075544085dba0856b5474dc8
ffg-training <internship0216to0222@gmail.com>
2025-09-02 16:12:21 +0900

    金融機関によって支店名が変わるようになった

--- bankTransfer/src/main/java/com/example/internship/controller/BankTransferController.java
+++ bankTransfer/src/main/java/com/example/internship/controller/BankTransferController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Controller
 public class BankTransferController {
@@ -18,13 +21,20 @@ public class BankTransferController {
 
     @GetMapping("/bankTransfer")
     public String bankTransfer(Model model) {
-        String[] bankName = {"山陰共同銀行", "海光共同銀行"};
-        String[] branchName = {"A支店","B支店"};
+        String[] bankName = {"", "山陰共同銀行", "海光共同銀行"};
+        String[] branchName1 = {"A支店","B支店"};
+        String[] branchName2 = {"C支店","D支店"};
+        Map<String, String[]> bankBranch = new HashMap<>();
+        bankBranch.put("山陰共同銀行", branchName1);
+        bankBranch.put("海光共同銀行", branchName2);
+
         String[] subjectName = {"普通","定期","当座","貯蓄"};
         model.addAttribute("bankTransferApplication", new BankTransferForm());
+        model.addAttribute("bankBranch", bankBranch);
         model.addAttribute("nameOptions", bankName);
-        model.addAttribute("nameOptions2", branchName);
-        model.addAttribute("nameOptions3", subjectName);
+//        model.addAttribute("branchName1", branchName1);
+//        model.addAttribute("branchName2", branchName2);
+        model.addAttribute("subjectName", subjectName);
         return "bankTransferMain";
     }
 
