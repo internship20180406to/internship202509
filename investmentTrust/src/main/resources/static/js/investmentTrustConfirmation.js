e3d24172901499f96c1a78606c4b41009100c85a
ffg-training <internship0216to0222@gmail.com>
2025-09-03 11:55:10 +0900

    修正

--- investmentTrust/src/main/resources/static/js/investmentTrustConfirmation.js
+++ investmentTrust/src/main/resources/static/js/investmentTrustConfirmation.js
@@ -1,4 +1,4 @@
 const submitButton = document.getElementById("submit")
     submitButton.addEventListener('click', (e) => {
-      console.log(confirm("操作を実行します"))
+      console.log(confirm("注文を完了します"))
     })
