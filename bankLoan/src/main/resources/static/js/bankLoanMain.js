4fd5be875ceefc0d22ee18de39838811d6ec850c
ffg-training <internship0216to0222@gmail.com>
2025-09-03 11:56:58 +0900

    金融、科目、住まいの種類を選択しないと先に進めない機能を追加

--- bankLoan/src/main/resources/static/js/bankLoanMain.js
+++ bankLoan/src/main/resources/static/js/bankLoanMain.js
@@ -2,10 +2,12 @@
     // セレクトボックスの値が変更されたときに呼ばれる関数
         function handlePlaceChange() {
             // セレクトボックスの選択された値を取得
-            var selectedBank = document.get("label").value;
+            var selectedBank = document.getElementById("bank").value;
+            var selectedBankKinds = document.getElementById("bankKinds").value;
+            var selectedHousingKinds = document.getElementById("housingKinds").value;
 
             // バリデーション処理
-            if (selectedBank !== "選択してください") {
+            if (selectedBank !== "選択してください" && selectedBankKinds !== "選択してください" && selectedHousingKinds !== "選択してください") {
             　　　　　　　// 「選択してください」以外が選ばれた場合はボタンを有効化
                         document.getElementById("submitButton").disabled = false;
                     } else {
@@ -13,3 +15,5 @@
                         document.getElementById("submitButton").disabled = true;
                     }
         }
+
+        function
